b57121edec603e5d98358882a758d5a01986d0ec
Mikhail <t9154360684@yandex.ru>
2026-09-09 20:48:06 +0300

    feat: initial project skeleton, packages and db properties

---
+++ src/main/java/ru/abrosimov/kinopoiskservice/KinopoiskServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.abrosimov.kinopoiskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinopoiskServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinopoiskServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/abrosimov/kinopoiskservice/KinopoiskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abrosimov.kinopoiskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinopoiskServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
